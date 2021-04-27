02e8dd501429288e60b4a3111ce52afcbe37fc82
高荣一 <459843853@qq.com>
2021-04-27 15:00:44 +0800

    INIT

--- src/main/java/cn/gaorongyi/split/entity/Test.java
+++ src/main/java/cn/gaorongyi/split/entity/Test.java
@@ -1,7 +1,5 @@
 package cn.gaorongyi.split.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -21,7 +19,6 @@ public class Test implements Serializable, Comparable<Test> {
 
     private static final long serialVersionUID = 1L;
 
-    @TableId(type = IdType.AUTO)
     private Long id;
 
     private String name;
